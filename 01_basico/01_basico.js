7f05b517de30af74d4ec62cccfd6a967364466d4
IBM-Wilber Donayres <wilberd29@gmail.com>
2018-01-27 15:29:02 -0500

    clase 1

---
+++ 01_basico/01_basico.js
@@ -0,0 +1,9 @@
+'use strict';
+console.log('Hola Node JS');
+console.log(2 + 5);
+//console.log(window);
+//console.log(global);
+
+setInterval(() => {
+    console.log('Hola ' + new Date() );
+}, 100);
